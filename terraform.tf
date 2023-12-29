8f86fbc74b8cabd88140087207c1a197e4284bac
turban <turban@uit-903217.lan>
2023-12-29 12:00:31 -0700

    Remove cloud block
      Committer: turban <turban@tim.urban@hotmail.com>

--- terraform.tf
+++ terraform.tf
@@ -1,13 +1,13 @@
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "turban_org"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
